af98f9001fb2db8d0aca6dabded709a342cc7ae3
Radoslaw Mejer <radmen@desmart.com>
2017-05-13 15:11:57 +0200

    Add console kernel

---
+++ bin/console.php
@@ -0,0 +1,18 @@
+<?php
+require(__DIR__.'/../vendor/autoload.php');
+
+$app = new \Illuminate\Container\Container();
+$console = new Illuminate\Console\Application($app, new Illuminate\Events\Dispatcher($app), 'dev-master');
+
+$app['config'] = [
+    'view.paths' => [__DIR__.'/../build/template'],
+    'view.compiled' => __DIR__.'/../build/template/compiled',
+];
+
+(new Illuminate\View\ViewServiceProvider($app))->register();
+(new Illuminate\Filesystem\FilesystemServiceProvider($app))->register();
+(new Illuminate\Events\EventServiceProvider($app))->register();
+
+$app->alias('view', \Illuminate\View\Factory::class);
+
+$console->run();
