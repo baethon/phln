1542a5aa472e78e2ec814816c8bbb4fc678c7aed
Radoslaw Mejer <radmen@desmart.com>
2017-06-30 20:34:09 +0200

    String support in collection\slice()

--- src/collection/slice.php
+++ src/collection/slice.php
@@ -3,32 +3,45 @@ declare(strict_types=1);
 
 namespace phln\collection;
 
-use const phln\fn\nil;
-use function phln\fn\curryN;
+use const phln\fn\{
+    __, nil, otherwise
+};
+use function phln\fn\{
+    curryN, partial, throwException
+};
+use function phln\type\typeCond;
 
 const slice = '\\phln\\collection\\slice';
 const 𝑓slice = '\\phln\\collection\\𝑓slice';
 
 /**
- * Extracts a slice of the array
+ * Extracts a slice of the array or string
  *
  * @phlnSignature Integer -> Integer -> [a] -> [a]
+ * @phlnSignature Integer -> Integer -> String -> String
  * @phlnCategory collection
- * @param string $offset
- * @param string $length
- * @param string $list
- * @return \Closure|mixed
- * @see \array_slice
+ * @param string|integer $offset
+ * @param string|integer $length
+ * @param string|array $collection
+ * @return \Closure|array|string
+ * @see \array_slice()
+ * @see \substr()
  * @example
  *      $takeTwo = \phln\collection\slice(0, 2);
  *      $takeTwo([1, 2, 3]); // [1, 2]
  */
-function slice($offset = nil, $length = nil, $list = nil)
+function slice($offset = nil, $length = nil, $collection = nil)
 {
-    return curryN(3, 𝑓slice, [$offset, $length, $list]);
+    return curryN(3, 𝑓slice, [$offset, $length, $collection]);
 }
 
-function 𝑓slice(int $offset, int $length, array $list): array
+function 𝑓slice(int $offset, int $length, $collection)
 {
-    return array_slice($list, $offset, $length);
+    $f = typeCond([
+        ['array', partial('\\array_slice', [__, $offset, $length])],
+        ['string', partial('\\substr', [__, $offset, $length])],
+        [otherwise, throwException(\InvalidArgumentException::class)],
+    ]);
+
+    return $f($collection);
 }
