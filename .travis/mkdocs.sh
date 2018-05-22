720f3ee64cbb4d92648937720b8ae7fd21475287
Radoslaw Mejer <radmen@desmart.com>
2018-05-22 20:24:05 +0200

    fixup! Travis: checkout gh-pages before gh-deploy

--- .travis/mkdocs.sh
+++ .travis/mkdocs.sh
@@ -7,6 +7,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 git remote add upstream "https://${GH_TOKEN}@github.com/baethon/phln.git"
+git fetch upstream
 
 git checkout upstream/gh-pages
 git checkout -
