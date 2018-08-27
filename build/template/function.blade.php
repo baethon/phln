7a6ed4004980a70e2a8dde7a0a10d3676d07fd59
Radoslaw Mejer <radmen@desmart.com>
2018-08-27 20:31:40 +0200

    Add dumym docblock when creating function
    Closes #34

--- build/template/function.blade.php
+++ build/template/function.blade.php
@@ -8,9 +8,18 @@ use function phln\fn\curryN;
 const {{$name}} = '\\phln\\{{$ns}}\\{{$name}}';
 const 𝑓{{$name}} = '\\phln\\{{$ns}}\\𝑓{{$name}}';
 
+/**
+ * Dummy description of {{$name}} function.
+ *
+ * @phlnSignature * -> *
+ * @phlnCategory {{$ns}}
+ * @return \Closure|mixed
+ * @example
+ *      \phln\{{$ns}}\{{$name}}();
+ */
 function {{$name}}()
 {
-    return curryN(N, 𝑓{{$name}});
+    return curryN(0, 𝑓{{$name}}, func_get_args());
 }
 
 function 𝑓{{$name}}()
