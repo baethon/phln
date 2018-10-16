97ab0c8e31f7b2fb8509fe7684be9867507f5c3f
Radoslaw Mejer <radmen@radmen.info>
2018-10-16 19:02:12 +0200

    Little rewrite of collapse()

--- src/macros/collection/collapse.php
+++ src/macros/collection/collapse.php
@@ -2,15 +2,16 @@
 declare(strict_types=1);
 
 use Baethon\Phln\Phln as P;
+use function Baethon\Phln\load_macro;
 
-P::macro('collapse', function (array $list): array {
-    return array_reduce(
-        $list,
-        function ($carry, $item) {
-            return is_array($item)
-                ? array_merge($carry, $item)
-                : P::append($item, $carry);
-        },
-        []
-    );
-});
+load_macro('collection', 'append');
+load_macro('collection', 'reduce');
+
+P::macro('collapse', P::reduce(
+    function ($carry, $item) {
+        return is_array($item)
+            ? array_merge($carry, $item)
+            : P::append($item, $carry);
+    },
+    []
+));
