a112779c2a5eed89b857e71578a06a62d5840a0f
Radoslaw Mejer <radmen@radmen.info>
2018-10-16 20:02:35 +0200

    Small refactor of last()

--- src/macros/collection/last.php
+++ src/macros/collection/last.php
@@ -2,19 +2,22 @@
 declare(strict_types=1);
 
 use Baethon\Phln\Phln as P;
+use function Baethon\Phln\load_macro;
 
-P::macro('last', function ($list) {
+load_macro('logic', 'isEmpty');
+
+P::macro('last', call_user_func(function () {
     $lastElement = P::slice(-1, 1);
     $lastOfArray = P::pipe([
         $lastElement,
         P::ifElse(P::isEmpty(), P::always(null), '\\current'),
     ]);
 
-    $f = P::typeCond([
-        ['array', $lastOfArray],
-        ['string', $lastElement],
-        [P::otherwise(), P::throwException(\InvalidArgumentException::class, [])],
-    ]);
-
-    return $f($list);
-});
+    return P::unary(
+        P::typeCond([
+            ['array', $lastOfArray],
+            ['string', $lastElement],
+            [P::otherwise(), P::throwException(\InvalidArgumentException::class, [])],
+        ])
+    );
+}));
