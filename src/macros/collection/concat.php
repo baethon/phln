978cf3a09b356a8cf058d407e4e888d17dfc2843
Radoslaw Mejer <radmen@radmen.info>
2018-10-16 19:19:20 +0200

    Small refactor of concat()

--- src/macros/collection/concat.php
+++ src/macros/collection/concat.php
@@ -2,22 +2,25 @@
 declare(strict_types=1);
 
 use Baethon\Phln\Phln as P;
+use function Baethon\Phln\load_macro;
 
-P::macro('concat', function ($a, $b) {
-    $getTypes = P::unapply(P::map('\\gettype'));
-    $matchesType = function (string $type) use ($getTypes) {
-        return P::compose([P::equals([$type, $type]), $getTypes]);
+load_macro('logic', 'cond');
+load_macro('collection', 'map');
+load_macro('relation', 'equals');
+load_macro('fn', ['T', 'unapply', 'compose', 'throwException']);
+
+P::macro('concat', call_user_func(function () {
+    $argsToTypes = P::unapply(P::map('\\gettype'));
+    $matchesType = function (string $type) use ($argsToTypes) {
+        return P::compose([P::equals([$type, $type]), $argsToTypes]);
     };
 
-    return P::apply(
-        P::cond([
-            [$matchesType('string'), P::curryN(3, '\\sprintf', ['%s%s'])],
-            [$matchesType('array'), P::curryN(2, '\\array_merge')],
-            [P::otherwise(), P::throwException(
-                \InvalidArgumentException::class,
-                ['Passed arguments are not supported']
-            )]
-        ]),
-        [$a, $b]
-    );
-});
+    return P::curryN(2, P::cond([
+        [$matchesType('string'), P::curryN(3, '\\sprintf', ['%s%s'])],
+        [$matchesType('array'), P::curryN(2, '\\array_merge')],
+        [P::otherwise(), P::throwException(
+            \InvalidArgumentException::class,
+            ['Passed arguments are not supported']
+        )]
+    ]));
+}));
--- src/macros/fn/compose.php
+++ src/macros/fn/compose.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 
 use Baethon\Phln\Phln as P;
+use function Baethon\Phln\load_macro;
+
+load_macro('fn', 'pipe');
 
 P::macro('compose', function (array $fns): \Closure {
     if (0 === count($fns)) {
