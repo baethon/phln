6cfb358b929be5bfb0bfdf78be1514580ad08b84
Radoslaw Mejer <radmen@radmen.info>
2018-10-16 20:00:24 +0200

    Small rewrite of head()

--- src/macros/collection/head.php
+++ src/macros/collection/head.php
@@ -2,25 +2,32 @@
 declare(strict_types=1);
 
 use Baethon\Phln\Phln as P;
+use function Baethon\Phln\load_macro;
 
-P::macro('head', function ($collection) {
+load_macro('fn', ['identity', 'unary']);
+load_macro('relation', 'lte');
+load_macro('logic', 'ifElse');
+load_macro('type', ['typeCond', 'is']);
+load_macro('collection', ['slice', 'length', 'nth']);
+
+P::macro('head', call_user_func(function () {
     $sliceFirst = P::slice(0, 1);
     $moreThanZero = P::compose([P::lte(1), P::length()]);
     $headOfArray = P::cond([
-        [$moreThanZero, P::compose(['\\current', $sliceFirst])],
+        [$moreThanZero, P::nth(0)],
         [P::otherwise(), P::always(null)],
     ]);
+
     $headOfString = P::cond([
         [$moreThanZero, $sliceFirst],
         [P::otherwise(), P::always('')],
     ]);
 
-    return P::apply(
+    return P::unary(
         P::typeCond([
             ['array', $headOfArray],
             ['string', $headOfString],
             [P::otherwise(), P::throwException(\InvalidArgumentException::class, [])],
-        ]),
-        [$collection]
+        ])
     );
-});
+}));
