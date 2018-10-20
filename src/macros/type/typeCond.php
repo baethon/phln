3bedac065333f6455fdb811c8709157b95143f2d
Radoslaw Mejer <radmen@radmen.info>
2018-10-20 18:31:27 +0200

    Small refactor of typeCond()

--- src/macros/type/typeCond.php
+++ src/macros/type/typeCond.php
@@ -2,14 +2,19 @@
 declare(strict_types=1);
 
 use Baethon\Phln\Phln as P;
+use function Baethon\Phln\load_macro;
 
-P::macro('typeCond', function (array $pairs): \Closure {
+load_macro('type', 'is');
+
+P::macro('typeCond', call_user_func(function () {
     $typeToPredicate = P::ifElse('\\is_callable', P::identity(), P::is());
-
     $mapRow = function ($row) use ($typeToPredicate) {
         $p = $typeToPredicate($row[0]);
         return [$p, $row[1]];
     };
 
-    return P::cond(array_map($mapRow, $pairs));
-});
+    return P::pipe([
+        P::map($mapRow),
+        P::cond()
+    ]);
+}));
