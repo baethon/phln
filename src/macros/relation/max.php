bebb6f8f44f391b26ddde99edeb8c39f4e429a6d
Radoslaw Mejer <radmen@radmen.info>
2018-10-20 18:26:32 +0200

    Small refactor of max()

--- src/macros/relation/max.php
+++ src/macros/relation/max.php
@@ -2,7 +2,8 @@
 declare(strict_types=1);
 
 use Baethon\Phln\Phln as P;
+use function Baethon\Phln\load_macro;
 
-P::macro('max', function ($a, $b) {
-    return max($a, $b);
-});
+load_macro('fn', 'binary');
+
+P::macro('max', P::binary('\\max'));
