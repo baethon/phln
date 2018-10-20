33a68339816494ba89b2f8ceba11622abb375436
Radoslaw Mejer <radmen@radmen.info>
2018-10-20 18:17:08 +0200

    Small refactor of unique()

--- src/macros/collection/unique.php
+++ src/macros/collection/unique.php
@@ -3,10 +3,14 @@ declare(strict_types=1);
 
 use Baethon\Phln\Phln as P;
 
-P::macro('unique', function (array $list): array {
+P::macro('unique', call_user_func(function () {
     $reducer = function ($carry, $item) {
-        return in_array($item, $carry, true) ? $carry : P::append($item, $carry);
+        return in_array($item, $carry, true)
+            ? $carry
+            : P::append($item, $carry);
     };
 
-    return array_reduce($list, $reducer, []);
-});
+    return function (array $list) use ($reducer): array {
+        return array_reduce($list, $reducer, []);
+    };
+}));
