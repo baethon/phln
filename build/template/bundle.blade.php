d9a6442c13249700f6295c0844e82d9f82f1dc27
Radoslaw Mejer <radmen@baethon.com>
2020-07-23 00:04:36 +0200

    Start the third revolution!

--- build/template/bundle.blade.php
+++
@@ -1,7 +0,0 @@
-{!! '<?'.'php' !!}
-
-use function Baethon\Phln\load_macro;
-
-@foreach($files as $item)
-load_macro('{{$item['ns']}}', '{{$item['name']}}');
-@endforeach
--- build/template/function.blade.php
+++
@@ -1,7 +0,0 @@
-{!! '<?'.'php' !!}
-declare(strict_types=1);
-
-use Baethon\Phln\Phln as P;
-
-P::macro('{{$name}}', function () {
-});
